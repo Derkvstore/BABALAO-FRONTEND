34a1ba951abf19a8024ff75455a32188024dc35e
Abdel Kader Diarra <abdelkaderdiarra707@gmail.com>
2025-08-11 20:14:25 +0000

    FRESH FRONTEND LOGIN

--- src/pages/Fournisseurs.jsx
+++ src/pages/Fournisseurs.jsx
@@ -30,7 +30,7 @@ export default function Fournisseurs() {
 
   // ✅ LOGIQUE CORRIGÉE POUR GÉRER LOCAL ET PRODUCTION
   const backendUrl = import.meta.env.PROD
-    ?'fresh-backned-production.up.railway.app'
+    ?'https://fresh-backned-production.up.railway.app'
 
     : 'http://localhost:3001';
 
